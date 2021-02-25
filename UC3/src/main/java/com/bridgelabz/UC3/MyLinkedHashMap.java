7c037ffb7768f3d5af802807cfda8f936b38d309
spatil022 <shamalpatil1998@gmail.com>
2021-02-25 22:39:18 +0530

    added UC3

---
+++ UC3/src/main/java/com/bridgelabz/UC3/MyLinkedHashMap.java
@@ -0,0 +1,67 @@
+package com.bridgelabz.UC3;
+
+
+import java.util.ArrayList;
+public class MyLinkedHashMap<K, V> {
+	private final int numBuckets;
+	ArrayList<MyLinkedList<K>> myBucketArray;
+	private MyHashMapNode<K, V> head;
+
+	public MyLinkedHashMap() {
+		this.numBuckets = 10;
+		this.myBucketArray = new ArrayList<>(numBuckets);
+		for (int i = 0; i < numBuckets; i++) {
+			this.myBucketArray.add(null);
+		}
+	}
+	private int getBucketIndex(K key) {
+		int hashCode = Math.abs(key.hashCode());
+		int index = hashCode % numBuckets;
+		return index;
+	}
+	public V get(K key) {
+		int index = this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList = this.myBucketArray.get(index);
+		if (myLinkedList == null)
+			return null;
+		MyHashMapNode<K, V> myMapNode = (MyHashMapNode<K, V>) myLinkedList.search(key);
+		return (myMapNode == null) ? null : myMapNode.getValue();
+	}
+	public void add(K key, V value) {
+		int index = this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList = this.myBucketArray.get(index);
+		if (myLinkedList == null) {
+			myLinkedList = new MyLinkedList<>();
+			this.myBucketArray.set(index, myLinkedList);
+		}
+		MyHashMapNode<K, V> myMapNode = (MyHashMapNode<K, V>) myLinkedList.search(key);
+		if (myMapNode == null) {
+			myMapNode = new MyHashMapNode<>(key, value);
+			myLinkedList.append(myMapNode);
+		} else {
+			myMapNode.setValue(value);
+		}
+	}
+
+	public MyHashMapNode<K,V> remove(K key) {
+		int index = this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList = this.myBucketArray.get(index);
+		if(myLinkedList==null)
+			return null;
+		else {
+			MyHashMapNode<K, V> myMapNode = (MyHashMapNode<K, V>) myLinkedList.search(key);
+			if(myMapNode==null) {
+				return null;
+			}
+			else {
+				MyHashMapNode<K, V> deletedNode = (MyHashMapNode<K, V>) myLinkedList.removeParticularNode(myMapNode);
+				return deletedNode;
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "MyLinkedHashMap List{"+ myBucketArray+ '}';
+	}
+}
---
+++ UC3/src/test/java/com/bridgelabz/UC3/MyLinkedHashMapTest.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.UC3;
+
+import org.junit.Assert;
+import org.junit.Test;
+public class MyLinkedHashMapTest {
+	@Test
+	public void giveASentence_whenWordsAreAddedToList_ShouldReturnWordFrequency() {
+		String sentence = "Paranoids are not paranoid because they are paranoid "+
+							"but because they keep putting themselves "+
+							"deliberately into paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for(String word : words) {
+			Integer value = myLinkedHashMap.get(word);
+			if(value == null)
+				value = 1;
+			else
+				value = value + 1;
+			myLinkedHashMap.add(word, value);
+		}
+		System.out.println(myLinkedHashMap);
+		int frequency = myLinkedHashMap.get("paranoid");
+		Assert.assertEquals(3, frequency);
+	}
+
+	@Test
+	public void givenASentence_whenWordsAreRemovedFromList_ShouldReturnRemovedWord() {
+		String sentence = "Paranoids are not paranoid because they are paranoid "+
+							"but because they keep putting themselves "+
+							"deliberately into paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = myLinkedHashMap.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			myLinkedHashMap.add(word, value);
+		}
+		myLinkedHashMap.remove("avoidable");
+		Integer frequency = myLinkedHashMap.get("avoidable");
+		Assert.assertEquals(null, frequency);
+	}
+}
